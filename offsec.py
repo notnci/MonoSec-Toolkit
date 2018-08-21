60dd380c2f2a1f92365069d453167c704e9ffcb4
Lol not me <32819549+entropicvector@users.noreply.github.com>
2018-08-21 13:02:29 -0400

    First major work done on offsec.py
    Added the hashcracking feature as well as dictionary building based off a text file. Passwords are linearly created for now.

--- offsec.py
+++ offsec.py
@@ -2,6 +2,10 @@
 #Made by Christian Krenzlin, Co-Founder of MonoSec
 #Special Thanks to Albert Slepak for his help
 import interface
+import itertools
+import math
+import hashlib
+import sys
 
 arr = ["Social Engineering  A collection of Social Engineering tools, including Phishing email producer, email spoofer, and many more","",
  "Dictionary Creation You can enter an array of words and/or numbers and the algorithm will create a list of all possible passwords created with those words and save it into your specified file path","",
@@ -10,12 +14,143 @@ arr = ["Social Engineering  A collection of Social Engineering tools, including 
  "Hash Cracking       This takes a different approach to cracking passwords. If you have a password hash from somewhere, and you'd like to know what the password is, but it may be in a one-way algorithm, you can use this with an attached wordlist to bruteforce the hash",
 ]
 
+arr = ["a"]
+def dictExec(list,idx):
+    arr.remove("a")
+    x = 0
+    with open(list) as f:
+        lines = [line.rstrip('\n') for line in open(list)]
+    for i in lines:
+        print(str(x)+": "+i)
+        arr.append(i)
+        x+=1
+    print()
+    print("Post value listing")
+    print()
+    y = 0
+    if(idx == 2):
+        for y in range(len(arr)):
+            for i in range(len(arr)):
+                print(arr[y] + arr[i])
+    elif(idx == 3):
+        for y in range(len(arr)):
+            for i in range(len(arr)):
+                for x in range(len(arr)):
+                    print(arr[y]+arr[i]+arr[x])
+    elif(idx == 4):
+        for y in range(len(arr)):
+            for i in range(len(arr)):
+                for x in range(len(arr)):
+                    for z in range(len(arr)):
+                        print(arr[y]+arr[i]+arr[x]+arr[z])
+
+    OffsecMenu()
+
+
+
+
+
+def crackExec(list,hash,algorithm):
+    arr.remove("a")
+    sentinel = hash
+    with open(list) as f:
+        lines = [line.rstrip('\n') for line in open(list)]
+    for i in lines:
+        arr.append(i)
+    if(algorithm == 1):
+        found = False
+        for i in range(len(arr)):
+            m = hashlib.md5(arr[i].encode())
+            print(m.hexdigest())
+            if(m.hexdigest() == hash):
+                print("The hash was found.The password is: "+arr[i])
+                found = True
+                break
+        if(not found):
+            print("No passwords matched")
+
+    elif(algorithm == 2):
+        found = False
+        for i in range(len(arr)):
+            m = hashlib.sha1(arr[i].encode())
+            print(m.hexdigest())
+            if(m.hexdigest() == hash):
+                print("The hash was found.The password is: "+arr[i])
+                found = True
+                break
+        if(not found):
+            print("No passwords matched")
+    elif(algorithm == 3):
+        found = False
+        for i in range(len(arr)):
+            m = hashlib.sha256(arr[i].encode())
+            print(m.hexdigest())
+            if(m.hexdigest() == hash):
+                print("The hash was found.The password is: "+arr[i])
+                found = True
+                break
+        if(not found):
+            print("No passwords matched")
+
+    elif(algorithm == 4):
+        found = False
+        for i in range(len(arr)):
+            m = hashlib.sha224(arr[i].encode())
+            print(m.hexdigest())
+            if(m.hexdigest() == hash):
+                print("The hash was found.The password is: "+arr[i])
+                found = True
+                break
+        if(not found):
+            print("No passwords matched")
+    elif(algorithm == 5):
+        found = False
+        for i in range(len(arr)):
+            m = hashlib.sha384(arr[i].encode())
+            print(m.hexdigest())
+            if(m.hexdigest() == hash):
+                print("The hash was found.The password is: "+arr[i])
+                found = True
+                break
+        if(not found):
+            print("No passwords matched")
+    elif(algorithm == 6):
+        found = False
+        for i in range(len(arr)):
+            m = hashlib.sha512(arr[i].encode())
+            print(m.hexdigest())
+            if(m.hexdigest() == hash):
+                print("The hash was found.The password is: "+arr[i])
+                found = True
+                break
+        if(not found):
+            print("No passwords matched")
+    else:
+        print("Not a valid option")
+        HashCrack()
+
+
+
+
 
 def SocialEngineering():
     print("asd")
 
 def Dictionary():
-    print("fdf")
+    print("You chose the dictionary creation tool. With this tool, you can create a password dictionary with just a few base words")
+    while True:
+        try:
+            list = input("Input your list name (has to be in the same directory as this): ")
+            with open(list) as f:
+                x = [line.rstrip('\n') for line in open(list)]
+            break
+        except OSError:
+            print("This file cannot be opened")
+    print()
+    idx = int(input("How many words would you want combined?(max 4): "))
+    while(idx > 4 or idx < 1 ):
+        idx = int(input("How many words would you want combined?(max 4): "))
+    dictExec(list,idx)
 
 def Brute():
     print("das")
@@ -24,7 +159,19 @@ def Reverse_Shell():
     print("dasdg")
 
 def HashCrack():
-    print("yeet")
+    print("You chose the hash cracking tool. With this tool, you'll be able to reverse check hashes in order to crack hashed passwords")
+    list = input("Input your list name (has to be in the same directory as this): ")
+    hash = input("Enter the hash you want cracked: ")
+    print()
+    print("Available Algorithms:")
+    print("1. md5")
+    print("2. sha1")
+    print("3. sha256")
+    print("4. sha224")
+    print("5. sha384")
+    print("6. sha512")
+    algorithm = int(input("Enter the number of the correct hashing algorithm: "))
+    crackExec(list,hash,algorithm)
 
 def OffsecHelpmenu():
     print("welcome to the help menu")
@@ -48,7 +195,6 @@ def OffsecMenu():
         print("SE")
         SocialEngineering()
     elif(x == 2):
-        print("Dictionary")
         Dictionary()
     elif(x == 3):
         print("BF")
@@ -66,5 +212,3 @@ def OffsecMenu():
         exit()
 
 OffsecMenu()
-
-        
